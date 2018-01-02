772ed346be673ca29485f5e64d78afc19ebe6545
Hari Bhrugubanda <hbhrugubanda@gmail.com>
2018-01-02 20:40:23 +1100

    Update for react and add stylesheets and fonts

--- src/App.js
+++ src/App.js
@@ -1,127 +1,106 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-no-target-blank */
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <body>
-      <div id="container">
-
-        <!-- Calculator body -->
-        <div class="cal-body">
-
-          <!-- Header -->
-          <div class="header">
-            <h6>Electronic Calculator</h6>
+        <div id='container'>
+          <div className='cal-body'>
+            <div className='header'>
+              <h6>Electronic Calculator</h6>
+            </div>
+            <div className='display'>
+              <div className='mem-input'>
+                0
+              </div>
+              <div className='act-input'>
+                0
+              </div>
+            </div>
+            <div className='buttons-all'>
+              <div className='container'>
+                <div className='row first-row'>
+                  <div className='col button-single button-special' id='AC'>
+                    AC
+                  </div>
+                  <div className='col button-single button-special' id='CE'>
+                    CE
+                  </div>
+                  <div className='col button-single' id='divide'>
+                    ÷
+                  </div>
+                  <div className='col button-single' id='multiply'>
+                    x
+                  </div>
+                </div>
+                <div className='row second-row'>
+                  <div className='col button-single'>
+                    7
+                  </div>
+                  <div className='col button-single'>
+                    8
+                  </div>
+                  <div className='col button-single'>
+                    9
+                  </div>
+                  <div className='col button-single' id='minus'>
+                    -
+                  </div>
+                </div>
+                <div className='row third-row'>
+                  <div className='col button-single'>
+                    4
+                  </div>
+                  <div className='col button-single'>
+                    5
+                  </div>
+                  <div className='col button-single'>
+                    6
+                  </div>
+                  <div className='col button-single' id='plus'>
+                    +
+                  </div>
+                </div>
+                <div className='row fourth-row'>
+                  <div className='col button-single'>
+                    1
+                  </div>
+                  <div className='col button-single'>
+                    2
+                  </div>
+                  <div className='col button-single'>
+                    3
+                  </div>
+                  <div className='col button-single' id='equals'>
+                    =
+                  </div>
+                </div>
+                <div className='row fifth-row'>
+                  <div className='col button-single'>
+                    0
+                  </div>
+                  <div className='col button-single'>
+                    0
+                  </div>
+                  <div className='col button-single'>
+                    .
+                  </div>
+                  <div className='col button-single' id='equals'>
+                    =
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-
-          <!-- Display -->
-
-          <div class="display">
-
-            <!-- Display: Memory Input -->
-
-            <div class="mem-input">
-              0
-            </div>
-
-            <!-- Display: Active Input -->
-
-            <div class="act-input">
-              0
-            </div>
-
-          </div> <!-- end of display -->
-
-          <!-- Buttons -->
-          <div class="buttons-all">
-          <div class="container">
-            <div class="row first-row">
-              <div class="col button-single button-special" id="AC">
-                AC
-              </div>
-              <div class="col button-single button-special" id="CE">
-                CE
-              </div>
-              <div class="col button-single" id="divide">
-                ÷
-              </div>
-              <div class="col button-single" id="multiply">
-                x
-              </div>
-            </div> <!-- end of first-row -->
-            <div class="row second-row">
-              <div class="col button-single">
-                7
-              </div>
-              <div class="col button-single">
-                8
-              </div>
-              <div class="col button-single">
-                9
-              </div>
-              <div class="col button-single" id="minus">
-                -
-              </div>
-            </div> <!-- end of second row -->
-            <div class="row third-row">
-              <div class="col button-single">
-                4
-              </div>
-              <div class="col button-single">
-                5
-              </div>
-              <div class="col button-single">
-                6
-              </div>
-              <div class="col button-single" id="plus">
-                +
-              </div>
-            </div> <!-- end of third-row -->
-            <div class="row fourth-row">
-              <div class="col button-single">
-                1
-              </div>
-              <div class="col button-single">
-                2
-              </div>
-              <div class="col button-single">
-                3
-              </div>
-              <div class="col button-single" id="equals">
-                =
-              </div>
-            </div> <!-- end of fourth-row -->
-            <div class="row fifth-row">
-              <div class="col button-single">
-                0
-              </div>
-              <div class="col button-single">
-                0
-              </div>
-              <div class="col button-single">
-                .
-              </div>
-              <div class="col button-single" id="equals">
-                =
-              </div>
-            </div> <!-- end of fifth-row -->
-          </div> <!-- end of container -->
-          </div> <!-- end of buttons-all -->
-
-        </div> <!-- end of cal-body -->
-
-        <!-- Footer section -->
-
-        <div class="footer container-fluid">
-          Designed and coded by <a class="emily-link" href="https://www.freecodecamp.com/whylime23" target="_blank">Emily Taylor</a> using CodePen and Chrome web developer tools.
-        </div> <!-- end of footer -->
-
-      </div> <!-- end of container -->
+          <div className='footer container-fluid'>
+            Designed and coded by <a className='emily-link' href='https://www.freecodecamp.com/whylime23' target='_blank'>Emily Taylor</a> using CodePen and Chrome web developer tools.
+          </div>
+        </div>
       </body>
-    );
+    )
   }
 }
 
-export default App;
+export default App
