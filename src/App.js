6cd46b1b465c09cba0e38407dac68882309d6484
Hari Bhrugubanda <hbhrugubanda@gmail.com>
2018-03-27 15:59:34 +1100

    Finished calculator

--- src/App.js
+++ src/App.js
@@ -2,99 +2,169 @@
 import React, { Component } from 'react'
 import './App.css'
 
+const OPERATORS = ['+', '/', '-', '*']
+// const NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+const isOperator = (value) => OPERATORS.includes(value);
+// const isNumber = (value) => NUMBERS.includes(value);
+
+const Button = (props) => {
+  const { value, handleClick, className } = props;
+  return (
+    <div className={ className || 'col button-single'} onClick={handleClick}>
+      { value }
+    </div>
+  );
+};
+
 class App extends Component {
+  state = {
+    calcMemory: "",
+    calcInput: "",
+    error: false
+  };
+
+  handleNumberClick = (value) => {
+    if (this.state.error) {
+      this.setState({
+        calcInput: value,
+        calcMemory: value,
+        error: false
+      });
+      return;
+    }
+    if (this.state.calcInput.length < 13) {
+      if (isOperator(this.state.calcInput[this.state.calcInput.length - 1])) {
+        this.setState({
+          calcInput: value,
+          calcMemory: this.state.calcMemory + value
+        });
+      }
+      else {
+        this.setState({
+          calcInput: this.state.calcInput + value,
+          calcMemory: this.state.calcMemory + value
+        });
+      }
+    }
+    else {
+      this.setState({
+        calcInput: "",
+        calcMemory: 'Digit limit met',
+        error: true
+      });
+    }
+  };
+
+  handleCE = () => {
+    this.setState ({
+      calcInput: ""
+    });
+  };
+
+  handleAC = () => {
+    this.setState ({
+      calcInput: "",
+      calcMemory: ""
+    });
+  };
+
+  handleOperator = (operator) => {
+    if (this.state.error) {
+      return;
+    }
+    if (!isOperator(this.state.calcMemory[this.state.calcMemory.length - 1])) {
+      this.setState ({
+        calcInput: operator,
+        calcMemory: this.state.calcMemory + operator
+      });
+    };
+  };
+
+  handleEquals = () => {
+    let answer = eval(this.state.calcMemory);
+    if (!Number.isInteger(answer)) {
+      answer = answer.toFixed(2)
+    }
+    this.setState ({
+      calcInput: answer
+    });
+  };
+
   render () {
     return (
-      <body>
-        <div id='container'>
-          <div className='cal-body'>
-            <div className='header'>
-              <h6>Electronic Calculator</h6>
+      <div id='container'>
+        <div className='cal-body'>
+          <div className='header'>
+            <h6>Electronic Calculator</h6>
+          </div>
+          <div className='display'>
+            <div className='mem-input'>
+              {this.state.calcMemory ? this.state.calcMemory : '0'}
             </div>
-            <div className='display'>
-              <div className='mem-input'>
-                0
-              </div>
-              <div className='act-input'>
-                0
-              </div>
-            </div>
-            <div className='buttons-all'>
-              <div className='container'>
-                <div className='row first-row'>
-                  <div className='col button-single button-special' id='AC'>
-                    AC
-                  </div>
-                  <div className='col button-single button-special' id='CE'>
-                    CE
-                  </div>
-                  <div className='col button-single' id='divide'>
-                    ÷
-                  </div>
-                  <div className='col button-single' id='multiply'>
-                    x
-                  </div>
-                </div>
-                <div className='row second-row'>
-                  <div className='col button-single'>
-                    7
-                  </div>
-                  <div className='col button-single'>
-                    8
-                  </div>
-                  <div className='col button-single'>
-                    9
-                  </div>
-                  <div className='col button-single' id='minus'>
-                    -
-                  </div>
-                </div>
-                <div className='row third-row'>
-                  <div className='col button-single'>
-                    4
-                  </div>
-                  <div className='col button-single'>
-                    5
-                  </div>
-                  <div className='col button-single'>
-                    6
-                  </div>
-                  <div className='col button-single' id='plus'>
-                    +
-                  </div>
-                </div>
-                <div className='row fourth-row'>
-                  <div className='col button-single'>
-                    1
-                  </div>
-                  <div className='col button-single'>
-                    2
-                  </div>
-                  <div className='col button-single'>
-                    3
-                  </div>
-                  <div className='col button-single' />
-                  <div className='button-single' id='equals'>
-                    =
-                  </div>
-                </div>
-                <div className='row fifth-row'>
-                  <div className='col-9 button-single button-zero'>
-                    0
-                  </div>
-                  <div className='col button-single'>
-                    .
-                  </div>
-                  <div className='col button-single' />
-                </div>
-              </div>
+            <div className='act-input'>
+              {this.state.calcInput ? this.state.calcInput : '0'}
             </div>
           </div>
-          <div className='footer container-fluid'>
-            Designed and coded by <a className='emily-link' href='https://www.freecodecamp.com/whylime23' target='_blank'>Emily Taylor</a> using CodePen and Chrome web developer tools.
+          <div className='buttons-all'>
+            <div className='container'>
+              <div className='row first-row'>
+                <div className='col button-single button-special' id='AC' onClick={() => this.handleAC()}>
+                  AC
+                </div>
+                <div className='col button-single button-special' id='CE' onClick={() => this.handleCE()}>
+                  CE
+                </div>
+                <div className='col button-single' id='divide' onClick={() => this.handleOperator('/')}>
+                  ÷
+                </div>
+                <div className='col button-single' id='multiply' onClick={() => this.handleOperator('*')}>
+                  x
+                </div>
+              </div>
+              <div className='row second-row'>
+                <Button value={7} handleClick={() => this.handleNumberClick('7')} />
+                <Button value={8} handleClick={() => this.handleNumberClick('8')} />
+                <Button value={9} handleClick={() => this.handleNumberClick('9')} />
+                <div className='col button-single' id='minus' onClick={() => this.handleOperator('-')}>
+                  -
+                </div>
+              </div>
+              <div className='row third-row'>
+                <Button value={4} handleClick={() => this.handleNumberClick('4')}/>
+                <Button value={5} handleClick={() => this.handleNumberClick('5')}/>
+                <Button value={6} handleClick={() => this.handleNumberClick('6')}/>
+                <div className='col button-single' id='plus' onClick={() => this.handleOperator('+')}>
+                  +
+                </div>
+              </div>
+              <div className='row fourth-row'>
+              <Button value={1} handleClick={() => this.handleNumberClick('1')}/>
+              <Button value={2} handleClick={() => this.handleNumberClick('2')}/>
+              <Button value={3} handleClick={() => this.handleNumberClick('3')}/>
+                <div className='col button-single' />
+                <div className='button-single' id='equals' onClick={this.handleEquals}>
+                  =
+                </div>
+              </div>
+              <div className='row fifth-row'>
+                <Button
+                  value={0}
+                  handleClick={() => this.handleNumberClick(0)}
+                  className="button-single col-9 button-zero"
+                />
+                <div className='col button-single' onClick={() => this.handleNumberClick('.')}>
+                  .
+                </div>
+                <div className='col button-single' />
+              </div>
+            </div>
           </div>
         </div>
-      </body>
+        <div className='footer container-fluid'>
+          Designed and coded by <a className='emily-link' href='https://www.freecodecamp.com/whylime23' target='_blank'>Emily Taylor</a> using CodePen and Chrome web developer tools.
+        </div>
+      </div>
     )
   }
 }
